22a4c5b1174c3a587269d19f3cb6cf89ec8e9b6e
Jones Ogolo <jay.topher11@gmail.com>
2020-12-06 20:06:12 +0100

    created error boundary

---
+++ src/exercise/ErrorBoundary.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {hasError: false, error: null}
+  }
+
+  static getDerivedStateFromError(error) {
+    // update state so next render shows fallback ui
+    return {hasError: true, error}
+  }
+
+  componentDidCatch(error, errorInfo) {
+    console.log(error, errorInfo)
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <this.props.FallbackComponent error={this.state.error} />
+    }
+    return this.props.children
+  }
+}
+
+export default ErrorBoundary
+
+export function ErrorFallback({error, resetErrorBoundary}) {
+  return (
+    <div role="alert">
+      There was an error:{' '}
+      <pre style={{whiteSpace: 'normal'}}>{error.message}</pre>
+      <button onClick={resetErrorBoundary}>Try Again</button>
+    </div>
+  )
+}
